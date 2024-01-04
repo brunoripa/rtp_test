a01f624a0ee2e099a0fb8f19838a3f088d45dc6d
Bruno Ripa <bruno.ripa@gmail.com>
2024-01-04 12:45:15 +0000

    Fix

--- mix.exs
+++ mix.exs
@@ -1,10 +1,11 @@
 defmodule RPT.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
   def project do
     [
       app: :rpt,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
