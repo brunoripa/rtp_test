81c5ce9d01f7e490ca872fdbb005642d485b0f08
Bruno Ripa <bruno.ripa@gmail.com>
2024-01-04 12:30:08 +0000

    Fix

--- lib/nif.ex
+++ lib/nif.ex
@@ -1,11 +1,15 @@
 defmodule RPT.Nif do
   @moduledoc false
 
+  @version "0.1.0"
+
   use RustlerPrecompiled,
     otp_app: :rpt,
     crate: "rustdep",
-    base_url: "https://github.com/brunoripa/rtp_test/releases/download/#{version}/librustdep.so",
-    version: "untagged-14c87d33d23a964b8926",
+    # base_url: "https://github.com/brunoripa/rtp_test/releases/download/v#{@version}",
+    base_url:
+      "https://github.com/brunoripa/rtp_test/actions/runs/7409228381/artifacts/1147448991",
+    version: "0.1.0"
 
   def add(a, b), do: :rustdep.add(a, b)
 end
